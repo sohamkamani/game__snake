3f04892ca22e012190fca1c8ffda88f7d9265caa
Soham Kamani <sohamkamani@gmail.com>
2017-12-16 19:37:24 +0530

    Moving snake without restrictions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import phaserImg from './assets/phaser.png'
 import bodyTile from './assets/tile.png'
 
+const Snake = require('./snake')
+
 window.PIXI = require('phaser-ce/build/custom/pixi')
 window.p2 = require('phaser-ce/build/custom/p2')
 window.Phaser = require('phaser-ce/build/custom/phaser-split')
@@ -13,7 +15,7 @@ window.onload = function () {
   //  Although it will work fine with this tutorial, it's almost certainly not the most current version.
   //  Be sure to replace it with an updated version before you start experimenting with adding your own code.
 
-  let tile
+  let tile, snake
   console.log(phaserImg)
   function preload () {
     game.load.image('logo', './' + phaserImg)
@@ -23,18 +25,26 @@ window.onload = function () {
   function create () {
     tile = game.add.sprite(game.world.centerX, game.world.centerY, 'body_tile')
     tile.anchor.setTo(0.5, 0.5)
+    snake = new Snake(15, 400, 40, 60)
+    snake.render(game)
   }
 
+  const snakeSpeed = 65
   const update = () => {
     if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
       tile.x -= speed
+      snake.move(snakeSpeed, 0)
     } else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
       tile.x += speed
+      snake.move(-snakeSpeed, 0)
     } else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+      snake.move(0, -snakeSpeed)
       tile.y += speed
     } else if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
       tile.y -= speed
+      snake.move(0, snakeSpeed)
     }
+    snake.render(game)
   }
   var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
     preload,
--- snake.js
+++ snake.js
@@ -1,4 +1,54 @@
-
-const Snake = function(game) {
-  
+const Snake = function (snakeLength, x, y, tileSize) {
+  this.tiles = []
+  this.ticker = 0
+  for (let i = 0; i < snakeLength; i += 1) {
+    let tile = {
+      x: x - i * tileSize,
+      y: y,
+      fresh: true
+    }
+    this.tiles.push(tile)
+    if (i === 0) {
+      this.head = tile
+    }
+  }
 }
+
+Snake.prototype.render = function (game) {
+  this.tiles.forEach((tile) => {
+    if (tile.fresh) {
+      tile.sprite = game.add.sprite(tile.x, tile.y, 'body_tile')
+      tile.fresh = false
+      return
+    }
+    tile.sprite.x = tile.x
+    tile.sprite.y = tile.y
+  })
+}
+
+const tickSpeed = 30
+Snake.prototype.tick = function(){
+  this.ticker += 1
+  if (this.ticker >= tickSpeed){
+    this.ticker = 0
+    return true
+  }
+  return false
+}
+
+Snake.prototype.move = function (speedX, speedY) {
+  if (!this.tick()){
+    return
+  }
+  let previous = this.head
+  this.tiles.forEach((tile, i) => {
+    const tmp = Object.assign({}, previous)
+    previous = Object.assign({},tile)
+    tile.x = tmp.x
+    tile.y = tmp.y
+  })
+  this.head.x += speedX
+  this.head.y += speedY
+}
+
+module.exports = Snake
